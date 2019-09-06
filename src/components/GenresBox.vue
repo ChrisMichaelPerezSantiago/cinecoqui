b250958d3c75086bf365a213c2363a4fbbf87464
Chris Michael <chris.perez@upr.edu>
2019-09-06 10:47:31 -0400

    :sparkles: more genres were added to the list.

--- src/components/GenresBox.vue
+++ src/components/GenresBox.vue
@@ -2,18 +2,46 @@
   <div>
     <div class="genre-list">
       <ul>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'accion'}}">accion</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'aventura'}}">aventura</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'comedia'}}">comedia</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'crimen'}}">crimen</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'drama'}}">drama</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'familia'}}">familia</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'fantasia'}}">fantasia</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'misterio'}}">misterio</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'suspense'}}">suspense</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'suspenso'}}">suspenso</router-link></li>
-        <li><router-link :to="{name: 'ByGenre' , params:{genres: 'terror'}}">terror</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'accion'}}">accion</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'accion-aventura'}}">accion-aventura</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'action'}}">action</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'action-adventure'}}">action-adventure</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'adventure'}}">adventure</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'animacion'}}">animacion</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'aventura'}}">aventura</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'belica'}}">belica</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'biografia'}}">biografia</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'ciencia-ficcion'}}">ciencia-ficcion</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'comedia'}}">comedia</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'comedy'}}">comedy</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'crimen'}}">crimen</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'deportes'}}">deportes</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'documental'}}">documental</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'drama'}}">drama</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'estrenos'}}">estrenos</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'familia'}}">familia</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'family'}}">family</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'fantasia'}}">fantasia</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'guerra'}}">guerra</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'historia'}}">historia</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'horror'}}">horror</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'misterio'}}">misterio</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'musica'}}">musica</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'mystery'}}">mystery</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'pelicula-de-la-television'}}">pelicula-de-la-television</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'pelicula-de-tv'}}">pelicula-de-tv</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'proximos-estrenos'}}">proximos-estrenos</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'reality'}}">reality</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'sci-fi-fantasy'}}">sci-fi-fantasy</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'soap'}}">soap</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'suspense'}}">suspense</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'suspenso'}}">suspenso</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'talk'}}">talk</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'terror'}}">terror</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'war-politics'}}">war-politics</router-link></li>
+        <li><router-link :to="{name: 'ByGenre' , params:{genres:'western'}}">western</router-link></li>        
       </ul>
     </div>
   </div>
 </template>
+
