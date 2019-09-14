257356b77ecc3bd3c02314231e1f611bd323e27a
Chris Michael <chris.perez@upr.edu>
2019-09-13 22:41:30 -0400

    Home template total_page value updated 193+

--- src/views/EpisodesSection.vue
+++ src/views/EpisodesSection.vue
@@ -45,7 +45,7 @@ export default {
     };
 
     const page = value(1);
-    const total_pages = value(188);
+    const total_pages = value(193);
 
     watch(() => 
       page.value , (value) => {
