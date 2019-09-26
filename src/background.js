e32d7a3b6771d64e0f810fb3fb2507bfc9a4a366
Chris Michael <chris.perez@upr.edu>
2019-09-26 11:16:51 -0400

    autoupdate added

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const url = require('url');
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow , autoUpdater } from 'electron'
 import {
   createProtocol,
   installVueDevtools
@@ -36,6 +36,34 @@ function createWindow () {
 
   win.setMenu(null);
 
+  const server = 'https://cinecoqui.chrismichael.now.sh'
+  const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+  autoUpdater.setFeedURL(feed);
+
+  setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 60000);
+
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+  
+    dialog.showMessageBox(dialogOpts, (response) => {
+      if (response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+
+  autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
+
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
@@ -51,6 +79,12 @@ function createWindow () {
     win = null
   })
 }
+
+const server = 'https://cinecoqui.chrismichael.now.sh'
+const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL(feed);
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
