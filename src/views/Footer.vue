f88243fd7d2de266b7eceab64860c417ac782ec0
Chris Michael <chris.perez@upr.edu>
2019-10-02 10:54:12 -0400

    app version added to in the footer template.

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -6,12 +6,14 @@
           
         </div>
         <div class="col-md-6 text-center">   
-         
+          
         </div>
       </div>
       <div class="justify-content-center text-center bottom">
         <span>© {{year}} CineCoqui , Todos los derechos reservados</span>
         <p class="font-italic">Ningúno de los vídeos se encuentra alojado en servidores.</p>
+        <br>
+        <p class="font-italic">v{{version}}</p>
         <a href="https://github.com/ChrisMichaelPerezSantiago/cinecoqui" class="btn btn-dark">
           <i class="fab fa-github"></i> 
           View code on Github
@@ -26,7 +28,8 @@
   export default {
     setup(){
       const values = {
-        year: new Date().getFullYear()
+        year: new Date().getFullYear(),
+        version: require('../../package.json').version
       };
       return{
         ...values
