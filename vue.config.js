2074b180297f9df8715e696b4d705dbff9a532bd
Chris Michael <chris.perez@upr.edu>
2019-10-04 15:40:25 -0400

    Customized configuration in pluginOptions for electronBuilder.

--- vue.config.js
+++ vue.config.js
@@ -3,12 +3,30 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        appId: "com.cinecoqui.app",
+        productName: "cinecoqui",
+        win: {
+          icon: "./public/logo.png",
+          target: [
+            {
+              target: "nsis",
+              arch: ["x64", "ia32"]
+            }
+          ]
+        },
+        nsis:{
+          oneClick: false,
+          perMachine: true,
+          allowToChangeInstallationDirectory: true
+        },
         publish: [
           {
             provider: "github",
             owner: "ChrisMichaelPerezSantiago",
             repo: "cinecoqui",
-            releaseType: "draft"
+            releaseType: "draft",
+            channel: "latest",
+            url: "cinecoqui.chrismichael.now.sh"
           }
         ]
       }
